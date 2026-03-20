3a4686e9d31a5b9231b32d0db879b44b990d35e0
Roman <69262893+kellhusCorp@users.noreply.github.com>
2026-03-20 18:58:45 +0400

    Add interfaces (#3)
    * Add interfaces
    * fix: add update method to job store

---
+++ src/AsyncJobScheduler.Application/Interfaces/IJobStore.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using AsyncJobScheduler.Domain.Entities;
+
+namespace AsyncJobScheduler.Application.Interfaces;
+
+/// <summary>
+/// Defines a job store.
+/// </summary>
+public interface IJobStore
+{
+    Job Add(Job job);
+    
+    bool TryGetJob(Guid id, [NotNullWhen(true)] out Job? job);
+    
+    IReadOnlyCollection<Job> Jobs { get; }
+    
+    bool TryUpdate(Job job);
+}
