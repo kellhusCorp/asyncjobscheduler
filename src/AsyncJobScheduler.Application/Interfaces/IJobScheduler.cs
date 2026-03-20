d04cbddf772c2a8463513f398f62a16c0b80e79c
kellhusCorp <kellhus.corp@gmail.com>
2026-03-20 18:38:48 +0400

    Add interfaces

---
+++ src/AsyncJobScheduler.Application/Interfaces/IJobScheduler.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using AsyncJobScheduler.Domain.Entities;
+
+namespace AsyncJobScheduler.Application.Interfaces;
+
+/// <summary>
+/// Defines a job scheduler.
+/// </summary>
+public interface IJobScheduler
+{
+    IReadOnlyCollection<Job> Jobs { get; }
+    
+    Job AddJob(TimeSpan duration, bool shouldFail, TimeSpan? timeout);
+
+    bool TryGetJob(Guid id, [NotNullWhen(true)] out Job? job);
+    
+    void CancelJob(Guid id);
+}
---
+++ src/AsyncJobScheduler.Application/Interfaces/IJobStore.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using AsyncJobScheduler.Domain.Entities;
+
+namespace AsyncJobScheduler.Application.Interfaces;
+
+/// <summary>
+/// Defines a job store.
+/// </summary>
+public interface IJobStore
+{
+    Job Add(Job job);
+    
+    bool TryGetJob(Guid id, [NotNullWhen(true)] out Job? job);
+    
+    IReadOnlyCollection<Job> Jobs { get; }
+}
