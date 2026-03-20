87abe367ca586d34e07ebf8f41e9062ee794b9c1
kellhusCorp <kellhus.corp@gmail.com>
2026-03-20 15:33:00 +0400

    Add job entity

---
+++ src/AsyncJobScheduler.Domain/Entities/Job.cs
@@ -0,0 +1,33 @@
+﻿using AsyncJobScheduler.Domain.Enums;
+
+namespace AsyncJobScheduler.Domain.Entities;
+
+/// <summary>
+/// Defines a job.
+/// </summary>
+public sealed class Job
+{
+    public Guid Id { get; set; }
+    
+    public JobStatus Status { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public DateTime? StartedAt { get; set; }
+    
+    public DateTime? FinishedAt { get; set; }
+    
+    public double ProgressPoints { get; set; }
+    
+    public string? ErrorMessage { get; set; }
+
+    #region It'll be defined by the user
+
+    public TimeSpan Duration { get; init; }
+    
+    public bool ShouldFail { get; init; }
+    
+    public TimeSpan? Timeout { get; init; }
+
+    #endregion
+}
---
+++ src/AsyncJobScheduler.Domain/Enums/JobStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace AsyncJobScheduler.Domain.Enums;
+
+/// <summary>
+/// Describes the status of a job.
+/// </summary>
+public enum JobStatus
+{
+    Created,
+    Queued,
+    Running,
+    Succeeded,
+    Failed,
+    Cancelled,
+    TimedOut
+}
