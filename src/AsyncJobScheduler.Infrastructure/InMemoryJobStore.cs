9bd74ac13903ae8cd5c38b5a776796d4b0d1a42c
kellhusCorp <kellhus.corp@gmail.com>
2026-03-20 19:09:37 +0400

    Add in-memory job store implementation

---
+++ src/AsyncJobScheduler.Infrastructure/InMemoryJobStore.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+using AsyncJobScheduler.Application.Interfaces;
+using AsyncJobScheduler.Domain.Entities;
+
+namespace AsyncJobScheduler.Infrastructure;
+
+/// <summary>
+/// Defines an in-memory job store.
+/// </summary>
+public sealed class InMemoryJobStore : IJobStore
+{
+    private readonly ConcurrentDictionary<Guid, Job> _jobs = new();
+    
+    public Job Add(Job job)
+    {
+        if (!_jobs.TryAdd(job.Id, job))
+        {
+            throw new InvalidOperationException("Job already exists");
+        }
+        
+        return job;
+    }
+
+    public bool TryGetJob(Guid id, [NotNullWhen(true)] out Job? job)
+    {
+        return _jobs.TryGetValue(id, out job);
+    }
+
+    public IReadOnlyCollection<Job> Jobs => _jobs.Values.ToList().AsReadOnly();
+
+    public bool TryUpdate(Job job)
+    {
+        if (!_jobs.ContainsKey(job.Id))
+        {
+            throw new InvalidOperationException("Job does not exist");
+        }
+
+        return _jobs.TryUpdate(job.Id, job, job);
+    }
+}
