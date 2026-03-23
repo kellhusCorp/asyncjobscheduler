206186f232985691b56b1748ebc8b434b9a0256b
kellhusCorp <kellhus.corp@gmail.com>
2026-03-23 13:48:06 +0400

    Added get job endpoint

--- src/AsyncJobScheduler.API/Program.cs
+++ src/AsyncJobScheduler.API/Program.cs
@@ -1,7 +1,11 @@
 using AsyncJobScheduler.API.Dtos;
+using AsyncJobScheduler.Application.Interfaces;
+using AsyncJobScheduler.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IJobStore, InMemoryJobStore>();
 
 builder.Services.AddOpenApi();
 
@@ -12,13 +16,25 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.MapPost("/api/jobs", ([FromBody]CreateJobRequest request) =>
+app.MapPost("/api/jobs", ([FromBody] CreateJobRequest request, [FromServices] IJobStore jobStore) =>
 {
     // todo implement
 
     var job = request.ToModel();
-    
+
+    jobStore.Add(job);
+
     return Results.Created($"/api/jobs/{job.Id}", job.ToResponse());
-}); 
+});
+
+app.MapGet("/api/jobs/{id:guid}", ([FromRoute] Guid id, [FromServices] IJobStore jobStore) =>
+{
+    if (!jobStore.TryGetJob(id, out var job))
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(job.ToResponse());
+});
 
 app.Run();
