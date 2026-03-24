622b991adbfe737368c24c651a01bd5946607e8f
kellhusCorp <kellhus.corp@gmail.com>
2026-03-24 13:22:40 +0400

    Updated openapi

--- src/AsyncJobScheduler.API/Program.cs
+++ src/AsyncJobScheduler.API/Program.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using AsyncJobScheduler.API.Dtos;
 using AsyncJobScheduler.API.Validators;
 using AsyncJobScheduler.Application.Interfaces;
@@ -17,26 +16,28 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapOpenApi("/openapi/{documentName}.yaml");
 }
 
 app.MapPost("/api/jobs", async (
-    [FromBody] CreateJobRequest request,
-    [FromServices] IJobStore jobStore,
-    [FromServices] IValidator<CreateJobRequest> validator) =>
-{
-    var validationResult = await validator.ValidateAsync(request);
-
-    if (!validationResult.IsValid)
+        [FromBody] CreateJobRequest request,
+        [FromServices] IJobStore jobStore,
+        [FromServices] IValidator<CreateJobRequest> validator) =>
     {
-        return Results.ValidationProblem(validationResult.ToDictionary());
-    }
+        var validationResult = await validator.ValidateAsync(request);
 
-    var job = request.ToModel();
+        if (!validationResult.IsValid)
+        {
+            return Results.ValidationProblem(validationResult.ToDictionary());
+        }
 
-    jobStore.Add(job);
+        var job = request.ToModel();
 
-    return Results.Created($"/api/jobs/{job.Id}", job.ToResponse());
-}).Produces<JobResponse>(StatusCodes.Status201Created);
+        jobStore.Add(job);
+
+        return Results.Created($"/api/jobs/{job.Id}", job.ToResponse());
+    }).Produces<JobResponse>(StatusCodes.Status201Created)
+    .Produces(StatusCodes.Status400BadRequest);
 
 app.MapGet("/api/jobs/{id:guid}", ([FromRoute] Guid id, [FromServices] IJobStore jobStore) =>
 {
@@ -46,7 +47,8 @@ app.MapGet("/api/jobs/{id:guid}", ([FromRoute] Guid id, [FromServices] IJobStore
     }
 
     return Results.Ok(job.ToResponse());
-}).Produces<JobResponse>();
+}).Produces<JobResponse>()
+.Produces(StatusCodes.Status404NotFound);
 
 app.MapGet("/api/jobs", ([FromServices] IJobStore jobStore) => { return Results.Ok(jobStore.Jobs.Select(x => x.ToResponse())); })
     .Produces<IEnumerable<JobResponse>>();
