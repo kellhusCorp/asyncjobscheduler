85d3d317d5e82dc0dcd014be7dab875c9bd161d5
Roman <69262893+kellhusCorp@users.noreply.github.com>
2026-03-24 14:43:08 +0400

    Add presentation layer (API) (#6)
    * Add mock create job endpoint
    * Add jobs requests
    * Added get job endpoint
    * Added get jobs endpoint
    * Removed todo comment
    * Updated openapi.yaml
    * Added a timespan validation attribute and updated openapi.yaml
    * Add fluentvalidation package
    * Add correct 201 status code to openapi
    * Updated openapi
    * Fixed smoke test
    * Removed https launch profile

---
+++ src/AsyncJobScheduler.API/Dtos/CreateJobRequest.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AsyncJobScheduler.Domain.Entities;
+using AsyncJobScheduler.Domain.Enums;
+
+namespace AsyncJobScheduler.API.Dtos;
+
+public sealed class CreateJobRequest
+{
+    [Required]
+    public TimeSpan Duration { get; init; }
+
+    public bool ShouldFail { get; init; }
+    
+    public TimeSpan? Timeout { get; init; }
+}
+
+public sealed record JobResponse(
+    Guid Id,
+    string Status,
+    double Progress,
+    DateTimeOffset CreatedAt,
+    DateTimeOffset? StartedAt,
+    DateTimeOffset? FinishedAt,
+    string? Error
+);
+
+public static class JobMapping
+{
+    public static JobResponse ToResponse(this Job job)
+    {
+        return new JobResponse(
+            job.Id,
+            job.Status.ToString(),
+            job.ProgressPoints,
+            job.CreatedAt,
+            job.StartedAt,
+            job.FinishedAt,
+            job.ErrorMessage
+        );
+    }
+
+    public static Job ToModel(this CreateJobRequest request)
+    {
+        return new Job
+        {
+            Id = Guid.NewGuid(),
+            CreatedAt = DateTime.UtcNow,
+            Duration = request.Duration,
+            ShouldFail = request.ShouldFail,
+            Status = JobStatus.Created,
+            Timeout = request.Timeout
+        };
+    }
+}
---
+++ src/AsyncJobScheduler.API/Program.cs
@@ -0,0 +1,56 @@
+using AsyncJobScheduler.API.Dtos;
+using AsyncJobScheduler.API.Validators;
+using AsyncJobScheduler.Application.Interfaces;
+using AsyncJobScheduler.Infrastructure;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IJobStore, InMemoryJobStore>();
+builder.Services.AddScoped<IValidator<CreateJobRequest>, CreateJobRequestValidator>();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapOpenApi("/openapi/{documentName}.yaml");
+}
+
+app.MapPost("/api/jobs", async (
+        [FromBody] CreateJobRequest request,
+        [FromServices] IJobStore jobStore,
+        [FromServices] IValidator<CreateJobRequest> validator) =>
+    {
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (!validationResult.IsValid)
+        {
+            return Results.ValidationProblem(validationResult.ToDictionary());
+        }
+
+        var job = request.ToModel();
+
+        jobStore.Add(job);
+
+        return Results.Created($"/api/jobs/{job.Id}", job.ToResponse());
+    }).Produces<JobResponse>(StatusCodes.Status201Created)
+    .Produces(StatusCodes.Status400BadRequest);
+
+app.MapGet("/api/jobs/{id:guid}", ([FromRoute] Guid id, [FromServices] IJobStore jobStore) =>
+{
+    if (!jobStore.TryGetJob(id, out var job))
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(job.ToResponse());
+}).Produces<JobResponse>()
+.Produces(StatusCodes.Status404NotFound);
+
+app.MapGet("/api/jobs", ([FromServices] IJobStore jobStore) => { return Results.Ok(jobStore.Jobs.Select(x => x.ToResponse())); })
+    .Produces<IEnumerable<JobResponse>>();
+
+app.Run();
